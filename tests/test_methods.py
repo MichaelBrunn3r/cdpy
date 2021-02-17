e60fc4d220b9da7e753ce787470a20040a37e4d3
Michael Brunner <MichaelBrunn3r@gmail.com>
2021-02-17 21:57:29 +0100

    Added tests for method response parsing

---
+++ tests/test_methods.py
@@ -0,0 +1,296 @@
+from typing import Generator
+
+import pytest
+
+import cdpy
+
+
+def execute_and_assert_yield(method, expected_yield):
+    method_json = method.send(None)
+
+    assert method_json == expected_yield
+    return method
+
+
+class TestCreateJson:
+    def test_builtin_list(self):
+        args = {"nodeId": cdpy.dom.NodeId(321), "forcedPseudoClasses": ["a", "b", "c"]}
+        expected = {
+            "method": "CSS.forcePseudoState",
+            "params": {"nodeId": 321, "forcedPseudoClasses": ["a", "b", "c"]},
+        }
+        r = cdpy.css.force_pseudo_state(**args)
+
+        assert "params" in r
+        assert "forcedPseudoClasses" in r["params"]
+        forcedPseudoClasses = r["params"]["forcedPseudoClasses"]
+        assert type(forcedPseudoClasses) == list
+        assert type(forcedPseudoClasses[0]) == str
+        assert forcedPseudoClasses[0] == "a"
+        assert r == expected
+
+    def test_typeless_enum(self):
+        args = {
+            "transferMode": "somemode",
+        }
+        expected = {"method": "Tracing.start", "params": {"transferMode": "somemode"}}
+        r = cdpy.tracing.start(**args)
+
+        assert r == expected
+
+    def test_unparse_simple(self):
+        args = {"nodeId": cdpy.dom.NodeId(123), "name": "aname", "value": "avalue"}
+        expected = {
+            "method": "DOM.setAttributeValue",
+            "params": {"nodeId": 123, "name": "aname", "value": "avalue"},
+        }
+        r = cdpy.dom.set_attribute_value(**args)
+
+        assert "params" in r
+        assert "nodeId" in r["params"]
+        assert type(r["params"]["nodeId"]) == int
+        assert r["params"]["nodeId"] == 123
+        assert r == expected
+
+    def test_simple_list(self):
+        args = {"nodeIds": [cdpy.dom.NodeId(222), cdpy.dom.NodeId(333)]}
+        expected = {
+            "method": "Overlay.getGridHighlightObjectsForTest",
+            "params": {"nodeIds": [222, 333]},
+        }
+        r = cdpy.overlay.get_grid_highlight_objects_for_test(**args)
+
+        assert issubclass(type(r), Generator)
+        r = next(r)
+        assert "params" in r
+        assert "nodeIds" in r["params"]
+        nodeIds = r["params"]["nodeIds"]
+        assert type(nodeIds) == list
+        assert type(nodeIds[0]) == int
+        assert nodeIds == [222, 333]
+        assert r == expected
+
+    def test_enum(self):
+        args = {"service": cdpy.background_service.ServiceName.BACKGROUND_SYNC}
+        expected = {
+            "method": "BackgroundService.startObserving",
+            "params": {"service": "backgroundSync"},
+        }
+        r = cdpy.background_service.start_observing(**args)
+
+        assert "params" in r
+        assert "service" in r["params"]
+        assert type(r["params"]["service"]) == str
+        assert r["params"]["service"] == "backgroundSync"
+        assert r == expected
+
+    def test_enum_list(self):
+        args = {
+            "permissions": [
+                cdpy.browser.PermissionType.GEOLOCATION,
+                cdpy.browser.PermissionType.NFC,
+            ]
+        }
+        expected = {
+            "method": "Browser.grantPermissions",
+            "params": {"permissions": ["geolocation", "nfc"]},
+        }
+        r = cdpy.browser.grant_permissions(**args)
+
+        assert "params" in r
+        assert "permissions" in r["params"]
+        permissions = r["params"]["permissions"]
+        assert type(permissions) == list
+        assert type(permissions[0]) == str
+        assert permissions[0] == "geolocation"
+        assert r == expected
+
+    def test_object(self):
+        args = {
+            "styleSheetId": cdpy.css.StyleSheetId("someid"),
+            "ruleText": "arule",
+            "location": cdpy.css.SourceRange(3, 0, 4, 5),
+        }
+        expected = {
+            "method": "CSS.addRule",
+            "params": {
+                "styleSheetId": "someid",
+                "ruleText": "arule",
+                "location": {
+                    "startLine": 3,
+                    "startColumn": 0,
+                    "endLine": 4,
+                    "endColumn": 5,
+                },
+            },
+        }
+        r = cdpy.css.add_rule(**args)
+
+        assert issubclass(type(r), Generator)
+        r = next(r)
+        assert "params" in r
+        assert "location" in r["params"]
+        location = r["params"]["location"]
+        assert type(location) == dict
+        assert type(location["startLine"]) == int
+        assert location["startLine"] == 3
+        assert r == expected
+
+    def test_object_list(self):
+        args = {
+            "propertiesToTrack": [
+                cdpy.css.CSSComputedStyleProperty("a", "vala"),
+                cdpy.css.CSSComputedStyleProperty("b", "valb"),
+            ]
+        }
+        expected = {
+            "method": "CSS.trackComputedStyleUpdates",
+            "params": {
+                "propertiesToTrack": [
+                    {"name": "a", "value": "vala"},
+                    {"name": "b", "value": "valb"},
+                ]
+            },
+        }
+        r = cdpy.css.track_computed_style_updates(**args)
+
+        assert "params" in r
+        assert "propertiesToTrack" in r["params"]
+        propertiesToTrack = r["params"]["propertiesToTrack"]
+        assert type(propertiesToTrack) == list
+        assert type(propertiesToTrack[0]) == dict
+        assert propertiesToTrack[0] == {"name": "a", "value": "vala"}
+        assert r == expected
+
+
+class TestReturn:
+    @pytest.fixture
+    def executed_add_script_method(self):
+        return execute_and_assert_yield(
+            cdpy.page.add_script_to_evaluate_on_load("abcdef"),
+            {
+                "method": "Page.addScriptToEvaluateOnLoad",
+                "params": {"scriptSource": "abcdef"},
+            },
+        )
+
+    @pytest.fixture
+    def executed_installability_errors_method(self):
+        return execute_and_assert_yield(
+            cdpy.page.get_installability_errors(),
+            {"method": "Page.getInstallabilityErrors", "params": {}},
+        )
+
+    @pytest.fixture
+    def executed_get_frame_tree_method(self):
+        return execute_and_assert_yield(
+            cdpy.page.get_frame_tree(), {"method": "Page.getFrameTree", "params": {}}
+        )
+
+    @pytest.fixture
+    def executed_capture_screenshot_method(self):
+        return execute_and_assert_yield(
+            cdpy.page.capture_screenshot(),
+            {"method": "Page.captureScreenshot", "params": {}},
+        )
+
+    @pytest.fixture
+    def executed_get_resource_content(self):
+        return execute_and_assert_yield(
+            cdpy.page.get_resource_content(cdpy.page.FrameId(22), "url"),
+            {
+                "method": "Page.getResourceContent",
+                "params": {"frameId": "22", "url": "url"},
+            },
+        )
+
+    def test_builtin(self, executed_capture_screenshot_method):
+        response_json = {"id": 432, "data": "some-data"}
+
+        with pytest.raises(StopIteration):
+            response = executed_capture_screenshot_method.send(response_json)
+
+            assert type(response) == str
+            assert response == "some-data"
+
+    def test_simple(self, executed_add_script_method):
+        response_json = {"id": 87634, "identifier": "anidentfier"}
+
+        with pytest.raises(StopIteration):
+            response = executed_add_script_method.send(response_json)
+
+            assert type(response) == cdpy.page.ScriptIdentifier
+            assert response == "anidentfier"
+
+    def test_object(self, executed_get_frame_tree_method):
+        response_json = {
+            "id": 11,
+            "frameTree": {
+                "frame": {
+                    "id": 123,
+                    "loaderId": 2,
+                    "url": "...",
+                    "domainAndRegistry": "...",
+                    "securityOrigin": "...",
+                    "mimeType": "...",
+                    "secureContextType": "Secure",
+                    "crossOriginIsolatedContextType": "Isolated",
+                    "gatedAPIFeatures": [],
+                }
+            },
+        }
+
+        with pytest.raises(StopIteration):
+            response = executed_get_frame_tree_method.send(response_json)
+
+            assert type(response) == cdpy.page.FrameTree
+            assert response.frame.id == 123
+
+    def test_list_of_objects(self, executed_installability_errors_method):
+        response_json = {
+            "id": 10,
+            "installabilityErrors": [
+                {
+                    "errorId": 1,
+                    "errorArguments": [
+                        {"name": "minimum-icon-size-in-pixels", "value": 64}
+                    ],
+                },
+                {"errorId": 5, "errorArguments": []},
+            ],
+        }
+
+        with pytest.raises(StopIteration):
+            response = executed_installability_errors_method.send(response_json)
+
+            assert type(response) == list
+            assert len(response) == 2
+            assert type(response[0]) == cdpy.page.InstallabilityError
+            assert response[0].errorId == 1
+            assert type(response[1]) == cdpy.page.InstallabilityError
+            assert response[1].errorId == 5
+
+    def test_return_multiple(self, executed_get_resource_content):
+        response_json = {"content": "lorem", "base64Encoded": False}
+
+        with pytest.raises(StopIteration):
+            response = executed_get_resource_content.send(response_json)
+
+            assert type(response) == dict
+            assert "content" in response
+            assert response["content"] == "lorem"
+            assert "base64Encoded" in response
+            assert response["base64Encoded"] == False
+
+    def test_pass_empty_response(self, executed_add_script_method):
+        with pytest.raises(KeyError, match=".*identifier.*"):
+            response = executed_add_script_method.send({})
+
+    def test_pass_no_id(self, executed_add_script_method):
+        response_json = {"identifier": "anidentfier"}
+
+        with pytest.raises(StopIteration):
+            response = executed_add_script_method.send(response_json)
+
+            assert type(response) == cdpy.page.ScriptIdentifier
+            assert response == "anidentfier"
